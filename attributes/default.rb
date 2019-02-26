d209844d3019c7623b1e10ae6b8bf150676b5bd8
Andrew Kener <andrew.kener@ibm.com>
2019-02-25 21:37:48 -0500

    removing old configure recipe and cleaning up default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,13 +4,10 @@
 default['logdna']['conf_key'] = nil
 default['logdna']['conf_logdir'] = nil
 default['logdna']['conf_logfile'] = nil
-default['logdna']['conf_exclude'] = nil
-default['logdna']['conf_config'] = nil
-default['logdna']['conf_hostname'] = nil
-default['logdna']['conf_tags'] = nil
-default['logdna']['conf_exclude_regex'] = nil
 default['logdna']['conf_apihost'] = nil
 default['logdna']['conf_loghost'] = nil
+default['logdna']['conf_tags'] = nil
+default['logdna']['conf_hostname'] = nil
 
 ## Agent State:
 default['logdna']['agent_install'] = true
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -5,15 +5,12 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 conf_key = node['logdna']['conf_key']
-conf_config = node['logdna']['conf_config']
 conf_logdir = node['logdna']['conf_logdir']
 conf_logfile = node['logdna']['conf_logfile']
-conf_exclude = node['logdna']['conf_exclude']
-conf_hostname = node['logdna']['conf_hostname']
-conf_tags = node['logdna']['conf_tags']
-conf_exclude_regex = node['logdna']['conf_exclude_regex']
 conf_apihost = node['logdna']['conf_apihost']
 conf_loghost = node['logdna']['conf_loghost']
+conf_tags = node['logdna']['conf_tags']
+conf_hostname = node['logdna']['conf_hostname']
 
 template '/etc/logdna.conf' do
   source 'logdna.conf.erb'
@@ -22,15 +19,12 @@ template '/etc/logdna.conf' do
   mode '0644'
   variables({
               config_key:           conf_key,
-              config_config:        conf_config,
               config_logdir:        conf_logdir,
               config_logfile:       conf_logfile,
-              config_exclude:       conf_exclude,
-              config_hostname:      conf_hostname,
-              config_tags:          conf_tags,
-              config_exclude_regex: conf_exclude_regex,
               config_apihost:       conf_apihost,
               config_loghost:       conf_loghost,
+              config_tags:          conf_tags,
+              config_hostname:      conf_hostname,
             })
   action :create
   notifies :restart, 'service[logdna-agent]', :delayed
